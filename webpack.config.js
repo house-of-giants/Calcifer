4529d5f21de714703949232a645147debc8d7737
Dominic Magnifico <dominicmagnifico@gmail.com>
2025-04-21 15:02:24 -0600

    Add Composer configuration and coding standards for Calcifer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const path = require( 'path' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
 
 module.exports = {
-    ...defaultConfig,
-    entry: {
-        index: path.resolve( process.cwd(), 'src', 'index.js' ),
-        'blocks/calculator/index': path.resolve( process.cwd(), 'src', 'blocks', 'calculator', 'index.js' ),
-    },
-    output: {
-        ...defaultConfig.output,
-        path: path.resolve( process.cwd(), 'build' ),
-    },
-}; 
+	...defaultConfig,
+	entry: {
+		index: path.resolve(process.cwd(), 'src', 'index.js'),
+		'blocks/calculator/index': path.resolve(
+			process.cwd(),
+			'src',
+			'blocks',
+			'calculator',
+			'index.js',
+		),
+	},
+	output: {
+		...defaultConfig.output,
+		path: path.resolve(process.cwd(), 'build'),
+	},
+};
